ea76a51601fd7e133f27f23848ce1a4785d43dd7
rihabbm7 <rrahouba7@gmail.com>
2020-11-12 20:25:17 +0100

    Data Binding

--- Desktop/5infob2/Angular/ConFusion/src/app/app.module.ts
+++ Desktop/5infob2/Angular/ConFusion/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import 'hammerjs';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-
+    MatButtonModule,
+    MatCardModule,
     MatGridListModule,
     MatListModule,
     BrowserModule,
